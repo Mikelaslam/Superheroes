758089b0f80cc950398a9f7f818daf09f6a900e3
MIKEL DAVIS <kipkorir385@gmail.com>
2023-03-27 23:06:22 +0300

    Show Powers/:id

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -3,4 +3,10 @@ class PowersController < ApplicationController
         powers = Power.all
         render json: powers
     end
+
+    def show
+    power=Power.find(params[:id])
+    render json: power
+    end
+
 end
