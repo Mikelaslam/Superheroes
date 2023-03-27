e98eea3c93d49b29953ed5b3b2bcfc5f68db16ef
MIKEL DAVIS <kipkorir385@gmail.com>
2023-03-27 23:17:17 +0300

    Posting hero_powers

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,2 +1,12 @@
 class HeroPowersController < ApplicationController
+    def create
+    hero_power = HeroPower.new(hero_power_params)
+
+    if hero_power.save
+      render json: hero_power, status: :created
+    else
+      render json: { error: hero_power.errors.full_messages.join(', ') }, status: :unprocessable_entity
+    end
+  end
+  
 end
